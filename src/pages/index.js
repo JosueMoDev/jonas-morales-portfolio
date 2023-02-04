2e49f06789216e68f9caeb9ac1cae44dd9b3fcf5
JosueMoDev <82752441+JosueMoDev@users.noreply.github.com>
2023-02-04 09:45:31 -0600

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function IndexPage() {
+  return <div className="text-3xl font-bold underline">Hello world!</div>
 }
