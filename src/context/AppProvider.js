fd35e321ac6533637f76b8d7317768ae07789819
JosueMoDev <82752441+JosueMoDev@users.noreply.github.com>
2023-04-12 08:51:55 -0600

    @fixture theme

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -3,29 +3,19 @@ import { appReducer } from './appReducer';
 import AppContext from '.';
 import React from 'react';
 
+
+const INITIAL_STATE = {
+    isIntroDone: false,
+    theme: null,
+    isDrawerOpen: false,
+    isEnLanguage: true
+}
+
+
 const AppProvider = ({ children }) => {
-    useEffect(() => { 
-        const systemTheme = (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-        if (localStorage.theme === 'dark' || systemTheme) {
-            INITIAL_STATE.theme = 'dark'
-            document.documentElement.classList.add('dark');
-            dispatch({type:'toggleTheme', payload:{theme:'dark'}})
-            if(systemTheme){ toggleTheme('dark')}
-        } else {
-            INITIAL_STATE.theme ='ligth'
-            document.documentElement.classList.remove('dark')
-            dispatch({type:'toggleTheme', payload:{theme:'light'}})
-          }
-    }, [])
-    const systemTheme = (window.matchMedia('(prefers-color-scheme: dark)').matches)?'dark':'light'
-    const themeSet = localStorage.theme
-    const INITIAL_STATE = {
-        isIntroDone: false,
-        theme: themeSet || systemTheme,
-        isDrawerOpen: false,
-        isEnLanguage: true
-    }
-    const [appState, dispatch] = useReducer( appReducer, INITIAL_STATE)
+    const [appState, dispatch] = useReducer(appReducer, INITIAL_STATE)
+    const system =(window.matchMedia('(prefers-color-scheme: dark)').matches)?'dark':'light'
+    appState.theme = localStorage.theme || system;
     const toggleTheme = (theme) => {
         dispatch({type:'toggleTheme', payload:{theme}})
     }
@@ -38,7 +28,17 @@ const AppProvider = ({ children }) => {
     const onToggleLanguage = () => { 
         dispatch({type:'onToggleLanguage'})
     }
-
+    useEffect(() => { 
+        const systemTheme = (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+        if (localStorage.theme === 'dark' || systemTheme ) {
+            document.documentElement.classList.add('dark');
+            dispatch({type:'toggleTheme', payload:{theme:'dark'}})
+            if(systemTheme){ toggleTheme('dark')}
+          } else {
+            document.documentElement.classList.remove('dark')
+            dispatch({type:'toggleTheme', payload:{theme:'light'}})
+          }
+    },[])
     return (
         <div className='flex-0'>
             <AppContext.Provider value={{ appState, toggleTheme, onIntroDone, onToggleDrawer, onToggleLanguage }}>{children}</AppContext.Provider>
